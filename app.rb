32cb5813d5d15022062a63d884d308aecadf1080
anyagunbergntijohanneberg <93259557+anyagunbergntijohanneberg@users.noreply.github.com>
2023-02-21 11:21:42 +0100

    Lagt till sjukdomssidan med länkarna till varje sjukdom, jag har också börjat styla sidan lite för att lättare se vad jag håller på med

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'slim'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+get('/') do
+    slim(:start)
+end
+
+get('/sjukdomar') do
+    db = SQLite3::Database.new("db/sjukdomar.db")
+    db.results_as_hash = true
+    result = db.execute('SELECT * FROM sjukdomar')
+    slim(:"sjukdomar/index",locals:{sjukdomar:result})
+end
