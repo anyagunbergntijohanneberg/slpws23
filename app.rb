77a656c750fb79347fc1e46eec48c315147f2562
anyagunbergntijohanneberg <93259557+anyagunbergntijohanneberg@users.noreply.github.com>
2023-02-28 11:23:11 +0100

    Idag har jag fixat så att man kan klicka på en sjukdom och få mer fakta om den specifika sjukdomen, har också börjat på funktionen som gör att jag kan lägga till sjukdomar. Nästa gång ska jag fortsätta på detta och eventuellt fixa med inloggningsfunktionen

--- app.rb
+++ app.rb
@@ -14,3 +14,24 @@ get('/sjukdomar') do
     slim(:"sjukdomar/index",locals:{sjukdomar:result})
 end
 
+get('/sjukdomar/new') do
+    slim(:"sjukdomar/new")
+end
+
+post('/sjukdomar/new') do
+    Namn = params[:Namn]
+    Sjuk_id = params[:Sjuk_id].to_i
+    p "Vi fick in datan #{Namn} och #{Sjuk_id}"
+    db = SQLite3::Database.new("db/sjukdomar.db")
+    db.execute("INSERT INTO sjukdomar (Namn, Sjuk_id) VALUES (?,?)",Namn, Sjuk_id)
+    redirect('/sjukdomar')
+end
+
+get('/sjukdomar/:id') do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/sjukdomar.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM sjukdomar WHERE Sjuk_id = ?",id).first
+    slim(:"sjukdomar/show",locals:{result:result})
+end
+
